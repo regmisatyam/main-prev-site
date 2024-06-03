40cb5ec5d0e5bdd430c3453cfab21e00e5c9ded7
SATYAM <155289674+regmisatyam@users.noreply.github.com>
2024-06-03 22:41:10 +0545

    Update credentials.php

--- projects/credentials.php
+++ projects/credentials.php
@@ -1,14 +1,28 @@
-<?php
-// MySQL database connection
-$servername = "sql101.infinityfree.com";
-$username = "epiz_34259072";
-$password = "7pYTVsKk9IKTzAN";
-$database = "epiz_34259072_project_links";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+<?php
+
+// MySQL database connection
+
+$servername = "sql101.infinityfree.com";
+
+$username = "epiz_[*hidden*]";//Replace with correct db username 
+
+$password = "7pYTVsK***k9IKT*]zAN";
+
+$database = "epiz_[*hidden*]_project_links";//Replace with correct db table name.
+
+
+
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+
+// Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
